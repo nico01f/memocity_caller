2f50c6ed11c94201f5e2bdbfaf94d322ab9d4a46
Nico Fuenzalida <nico01f@gmail.com>
2016-09-19 19:55:55 -0300

    connection to yaml file

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
 require 'pg'
 require 'date'
+require 'yaml'
+
 
 hoy = Time.now.strftime("%Y-%m-%d")
-conn = PG.connect :dbname => 'd814pral4f35oh', :user => 'fwgussdvvrmoia', :host=> 'ec2-54-83-3-38.compute-1.amazonaws.com', :password => 'wja1vjCs2pWx-EiKBaggGxo5eq'
+
+config = YAML.load_file("database.yaml")
+
+conn = PG.connect :dbname => "#{config["config"]["dbname"]}", :user => "#{config["config"]["user"]}", :host=> "#{config["config"]["host"]}", :password => "#{config["config"]["password"]}"
 
 
 result = conn.exec("select reminders.user_id, reminders.when_activate, reminders.content, users.mobile_num from reminders join users on reminders.user_id = users.id where when_activate::text like '#{hoy}%';")
