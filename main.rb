80ccf26f6eb165e43a2722da0c7241238bda4bca
Nico Fuenzalida <nico01f@gmail.com>
2016-09-19 20:09:05 -0300

    remove def

--- main.rb
+++ main.rb
@@ -7,15 +7,13 @@ hoy = Time.now.strftime("%Y-%m-%d")
 
 config = YAML.load_file("database.yaml")
 
-def dbconfig
-  @dbname = config["database"]["dbname"]
-  @user = config["database"]["user"]
-  @host = config["database"]["host"]
-  @password = config["database"]["password"]
+dbname = config["database"]["dbname"]
+user = config["database"]["user"]
+host = config["database"]["host"]
+password = config["database"]["password"]
 
-end
 
-conn = PG.connect :dbname => "#{@dbname}", :user => "#{@user}", :host=> "#{@host}", :password => "#{@password}"
+conn = PG.connect :dbname => "#{dbname}", :user => "#{user}", :host=> "#{host}", :password => "#{password}"
 
 
 result = conn.exec("select reminders.user_id, reminders.when_activate, reminders.content, users.mobile_num from reminders join users on reminders.user_id = users.id where when_activate::text like '#{hoy}%';")
